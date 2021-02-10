4d54162655eec372f3e27ea08e37a22ed37dd88b
Guilherme Reis <guilhermeureis@gmail.com>
2021-02-10 10:33:47 -0300

    Alertas, Keyboard e Login com campo randômico.

---
+++ spec/alerts_spec.rb
@@ -0,0 +1,45 @@
+describe 'Alertas de JS', :alerts do
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/javascript_alerts'
+    end
+
+    it 'alerta' do
+        click_button 'Alerta'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'Isto é uma mensagem de alerta'
+    end
+
+    it 'sim confirma' do
+        click_button 'Confirma'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+        sleep 2
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'Mensagem confirmada'
+    end
+    
+    it 'não confirma' do
+        click_button 'Confirma'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content 'Mensagem não confirmada'
+    end
+
+    it 'accept prompt', :accept_prompt do
+        accept_prompt(with: 'Fernando') do
+            click_button 'Prompt'
+            sleep 2
+        end
+        expect(page).to have_content 'Olá, Fernando'
+        sleep 3
+    end
+
+    it 'dismiss prompt' do
+        click_button 'Prompt'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'Qual o seu Nome?'
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content 'Olá, null'
+    end
+end
---
+++ spec/key_spec.rb
@@ -0,0 +1,42 @@
+describe 'Teclado', :key do
+    
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/key_presses'
+    end
+
+    it 'enviando tecla espaço' do
+        # Simulando o espaço
+        find('#campo-id').send_keys :space
+        expect(page).to have_content 'You entered: SPACE'
+        sleep 3
+    end
+
+    it 'enviando tecla TAB' do
+        # Simulando o espaço
+        find('#campo-id').send_keys :tab
+        expect(page).to have_content 'You entered: TAB'
+        sleep 3
+    end
+
+    it 'enviando teclas de simbolos' do
+        # Array de simbolo => %i
+        teclas = %i[tab escape space enter shift control alt]
+
+        teclas.each do |t|
+            find('#campo-id').send_keys t
+            expect(page).to have_content 'You entered: ' + t.to_s.upcase
+            sleep 1
+        end
+    end
+
+    it 'enviando teclas de letras' do
+        # Array de simbolo => %i
+        letras = %w[a s d f g h j k l]
+
+        letras.each do |l|
+            find('#campo-id').send_keys l
+            expect(page).to have_content 'You entered: ' + l.to_s.upcase
+            sleep 1
+        end
+    end
+end
